39b914c6c8ec5154aa73a77261310d3a8d6bf17e
Antonio Mancera Ubaldo Jr. <142708326+JunSayke@users.noreply.github.com>
2024-12-22 00:19:56 +0800

    [Hunyo] Refactor manga fetching logic to use a dedicated URL and store latest mangas in an array

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const cheerio = require("cheerio")
 const axios = require("axios")
 
-const url = "https://mangakakalot.com/"
+const domain = "https://mangakakalot.com/"
+const mangaListUrl = `${domain}manga_list?type=latest&category=all&state=all&page=1`
+const latestMangas = []
 
+// https://www.youtube.com/watch?v=-ZMwRnxIxZY
+// TODO: Handle pagination
+// TODO: Prevent being blocked by the website
 async function getLatest() {
 	try {
-		const response = await axios.get(url)
+		const response = await axios.get(mangaListUrl)
 		const $ = cheerio.load(response.data)
-		const test = $(
-			"body > div.container > div.main-wrapper > div.leftCol > div.daily-update > h2"
-		).text()
+		const mangas = $("div.list-truyen-item-wrap")
 
-		console.log(test)
+		mangas.each(function () {
+			const title = $(this).find("h3 a").text()
+			const thumbnail = $(this).find("a img").attr("src")
+			const mangaLink = $(this).find("h3 a").attr("href")
+
+			latestMangas.push({
+				title: title,
+				thumbnail: thumbnail,
+				link: mangaLink,
+			})
+		})
+		console.log(latestMangas)
 	} catch (error) {
 		console.error(error)
 	}
