5da8e1c28eb9e4be86a55e48bcabe44d8d2985a3
Antonio Mancera Ubaldo Jr. <142708326+JunSayke@users.noreply.github.com>
2025-01-05 23:56:31 +0800

    [Hunyo] Moved createChapter helper to MangaChapter.ts, moved createManga helper to Manga.ts

--- src/types/Manga.ts
+++ src/types/Manga.ts
@@ -6,12 +6,69 @@ type Manga = {
 	link: string // identifier for the manga
 	title: string
 	synopsis: string | null // Null if not available
-	thumbnailUrl: string | null // Null if not available
+	thumbnail: string | null // Null if not available
+	author: string[] | null // Null if not available
 	genres: string[] | null // Null when fetching only for manga info and not the manga content itself
 	status: string | null // Null if not available
-	rating: number | null // Rating out of 5 Null if not available
+	rating: number | null // Rating out of 5 normalize if needed, Null if not available
 	views: number | null // Null if not available
 	chapters: MangaChapter[] | null // Null when fetching only for manga info and not the manga content itself
 }
 
 export default Manga
+
+/**
+ * Helper function to create a Manga object.
+ * Creates a Manga object with the given parameters. Automatically assigns null to optional parameters if not provided.
+ *
+ * @param {Object} params - The parameters for creating the Manga object.
+ * @param {string} params.link - The link to the manga.
+ * @param {string} params.title - The title of the manga.
+ * @param {string} [params.synopsis] - (Optional) The synopsis of the manga. Defaults to null if not provided.
+ * @param {string} params.thumbnailUrl - (Optional) The thumbnail URL of the manga. Defaults to null if not provided.
+ * @param {string[]} [params.genres] - (Optional) The genres of the manga. Defaults to null if not provided.
+ * @param {string} [params.status] - (Optional) The status of the manga. Defaults to null if not provided.
+ * @param {number} [params.rating] - (Optional) The rating of the manga. Defaults to null if not provided.
+ * @param {number} [params.views] - (Optional) The views of the manga. Defaults to null if not provided.
+ * @param {MangaChapter[]} [params.chapters] - (Optional) The chapters of the manga. Defaults to null if not provided.
+ * @returns {Manga} - The created Manga object.
+ * @throws {Error} - Throws an error if the link or title is not provided or undefined.
+ */
+export function createManga({
+	link,
+	title,
+	synopsis = null,
+	thumbnail = null,
+	author = null,
+	genres = null,
+	status = null,
+	rating = null,
+	views = null,
+	chapters = null,
+}: {
+	link: string
+	title: string
+	synopsis?: string | null
+	thumbnail?: string | null
+	author?: string[] | null
+	genres?: string[] | null
+	status?: string | null
+	rating?: number | null
+	views?: number | null
+	chapters?: MangaChapter[] | null
+}): Manga {
+	// if (!link) throw new Error("Cannot find manga link")
+	// if (!title) throw new Error("Cannot find manga title")
+	return {
+		link: link,
+		title: title,
+		synopsis: synopsis,
+		thumbnail: thumbnail,
+		author: author,
+		genres: genres,
+		status: status,
+		rating: rating,
+		views: views,
+		chapters: chapters,
+	}
+}
--- src/types/MangaChapter.ts
+++ src/types/MangaChapter.ts
@@ -1,8 +1,41 @@
 type MangaChapter = {
 	link: string // identifier for the chapter
 	title: string
-	date: Date | null // Null when fetching only for chapter info and not the chapter content itself
+	date: string | null // Null when fetching only for chapter info and not the chapter content itself
 	images: string[] | null // Null when fetching only for chapter info and not the chapter content itself
 }
 
 export default MangaChapter
+
+/**
+ * Helper function to create a MangaChapter object.
+ * Creates a MangaChapter object with the given parameters. Automatically assigns null to optional parameters if not provided.
+ *
+ * @param {Object} params - The parameters for creating the MangaChapter object.
+ * @param {string} params.link - The link to the chapter.
+ * @param {string} params.title - The title of the chapter.
+ * @param {string} [params.date] - (Optional) The date of the chapter. Defaults to null if not provided.
+ * @param {string} [params.images] - (Optional) The chapter images. Defaults to null if not provided.
+ * @returns {MangaChapter} - The created MangaChapter object.
+ * @throws {Error} - Throws an error if the link or title is not provided or undefined.
+ */
+export function createChapter({
+	link,
+	title,
+	date = null,
+	images = null,
+}: {
+	link: string
+	title: string
+	date?: string | null
+	images?: string[] | null
+}): MangaChapter {
+	if (!link) throw new Error("Cannot find chapter link")
+	if (!title) throw new Error("Cannot find chapter title")
+	return {
+		link: link,
+		title: title,
+		date: date,
+		images: images,
+	}
+}
